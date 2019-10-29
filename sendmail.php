62516a9c831e58885d0a57a9a12de50913c34271
zuhairbadawi <badawizuhair@gmail.com>
2019-10-29 21:34:56 +0200

    My Resume

---
+++ sendmail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Call Back: $call \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+$recipient = "badawizuhair@gmail.com";
+$subject = "Message from Your Website";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
